75a85529678c691fc2d5d1a9a36fa3d180d377bd
Timothy Wu <timothyjwu1@gmail.com>
2019-04-18 18:15:12 -0400

    fix sound autoplay

---
+++ src/js/fish.js
@@ -0,0 +1,9 @@
+class Fish {
+  constructor(ctx, canvasWidth, canvasHeight, map) {
+    this.ctx = ctx;
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.map = map;
+  }
+}
+export default Fish;
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Map from "./map";
 import Player from "./player";
+import Fish from "./fish";
 import { Howl, Howler } from "howler";
 
 Howler.volume(0.6);
@@ -33,53 +34,56 @@ const mainSong = new Howl({
   // autoplay: true,
   loop: true,
   autoUnlock: true,
-  volume: 0.6,
-
+  volume: 0.6
 });
 const feintSong = new Howl({
   src: ["./src/songs/feint.mp3"],
   loop: false,
   autoUnlock: true,
-  volume: 0.6,
+  volume: 0.6
 });
+
 startSong.play();
-// startSong.on("fade", () => mainSong.play);
+
 let map = new Map(ctx, canvasWidth, canvasHeight);
-let player = new Player(ctx, canvasWidth, canvasHeight, map, changeState,feintSong);
+let player = new Player(
+  ctx,
+  canvasWidth,
+  canvasHeight,
+  map,
+  changeState,
+  feintSong
+);
+let fish = new Fish(ctx, canvasWidth, canvasHeight, map);
 
 const unmute = document.getElementById("vol-on");
 const mute = document.getElementById("vol-off");
 const volume = document.querySelector("#volume");
 volume.addEventListener("click", () => {
-  if(mainSong.playing()){
-    mainSong.pause();
-    mainSong.mute(true);
-  }else if(!mainSong.playing()){
-    mainSong.mute(false);
-    mainSong.play();
-  }
-  if (startSong.playing()) {
-    startSong.pause();
-    startSong.mute(true);
-  } else if (!startSong.playing()) {
-    startSong.play();
-    startSong.mute(false);
-  }
-  // mainSong.mute() ? mainSong.mute(false) : mainSong.mute(true);
-  // mainSong.playing() ? mainSong.pause() mainSong.mute(false); : mainSong.play();
-  // startSong.mute() ? startSong.mute(false) : startSong.mute(true);
-  // startSong.playing() ? startSong.pause() : startSong.play();
+  // if(mainSong.playing()){
+  //   mainSong.pause();
+  //   mainSong.mute(true);
+  // }else if(!mainSong.playing()){
+  //   mainSong.mute(false);
+  //   mainSong.play();
+  // }
+  // if (startSong.playing()) {
+  //   startSong.pause();
+  //   startSong.mute(true);
+  // } else if (!startSong.playing()) {
+  //   startSong.play();
+  //   startSong.mute(false);
+  // }
+  mainSong.mute() ? mainSong.mute(false) : mainSong.mute(true);
+  mainSong.playing() ? mainSong.pause() : mainSong.play();
+  startSong.mute() ? startSong.mute(false) : startSong.mute(true);
+  startSong.playing() ? startSong.pause() : startSong.play();
   unmute.classList.toggle("hidden");
   mute.classList.toggle("hidden");
 });
 document.addEventListener("keydown", player.keyDownHandler);
 document.addEventListener("keyup", player.keyUpHandler);
 const endScreen = document.getElementById("end-screen");
-// const playing = setInterval(() => {
-//   if(state == 1){
-//     // mainSong.play();
-//   }
-// }, 10);
 const startBtn = document.getElementById("new-game-btn");
 const startScreen = document.getElementById("start-screen");
 startBtn.addEventListener("click", () => {
@@ -91,9 +95,9 @@ let animation;
 const animate = () => {
   switch (state) {
     case 1:
-      // if (!mainSong.playing()) {
-      //   mainSong.play();
-      // }
+      if (!mainSong.playing()) {
+        mainSong.play();
+      }
 
       ctx.clearRect(0, 0, canvasWidth, canvasHeight);
       map.render();
@@ -102,7 +106,7 @@ const animate = () => {
       player.checkTiles();
       player.checkObs();
       break;
-      case 2:
+    case 2:
       ctx.clearRect(0, 0, canvasWidth, canvasHeight);
       player.sprite.loop();
       // player.checkObs();
@@ -111,10 +115,10 @@ const animate = () => {
       document.removeEventListener("keyup", player.keyUpHandler);
       break;
     case 3:
-    feintSong.play();
-    state = 2;
-    break;
-    }
+      feintSong.play();
+      state = 2;
+      break;
+  }
   animation = requestAnimationFrame(animate);
 };
 animate();
